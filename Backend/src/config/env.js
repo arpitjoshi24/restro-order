5fd2b10c19290f6c4ac385368e1c0f4477072111
Arpit_Joshi <134071763+arpitjoshi24@users.noreply.github.com>
2026-06-01 10:06:03 +0530

    Delete Backend/src/config/env.js

--- Backend/src/config/env.js
+++
@@ -1,3 +0,0 @@
-DATABASE_URL="postgresql://neondb_owner:npg_w1zSjKUlb6Hs@ep-spring-union-ap8lyeow-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-JWT_SECRET="supersecret"
-PORT=5000
